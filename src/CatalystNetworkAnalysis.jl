bc0053741bf690586a6c4d9244f97aef74f83db8
vyudu <vincent.duyuan@gmail.com>
2024-06-03 19:10:22 -0400

    up

--- src/CatalystNetworkAnalysis.jl
+++ src/CatalystNetworkAnalysis.jl
@@ -4,4 +4,9 @@ using Catalyst
 using Satisfiability
 using Oscar
 
+import ModelingToolkit as MT
+
+include("persistence.jl")
+export ispersistent
+
 end
--- src/persistence.jl
+++ src/persistence.jl
@@ -6,12 +6,16 @@
 
 function ispersistent(rs::ReactionSystem) 
     conslaws = conservationlaws(rs)
-    conservative = !isempty(conservationlaws(rs))
-    consistent = !isempty(cycles(rs))
+    cyclemat = Catalyst.cycles(rs)
+    conservative = !isempty(conslaws) && 
+        any(row->all(x->x>=0, row), eachrow(conslaws))
+    consistent = !isempty(cyclemat) &&
+        any(col->all(x->x>=0, col), eachcol(cyclemat))
 
     siphons = minimalsiphons(rs)
-    conservative && consistent && all(!iscritical(s, conslaws), siphons)
+    conservative && consistent && all(s->!iscritical(s, conslaws), siphons)
 end
+
 
 """
     minimalsiphons(rs::ReactionSystem)
@@ -19,15 +23,15 @@ end
     Constructs the set of minimal siphons of a reaction network, where a siphon is a set of species that can be "switched off," i.e. if the species each have concentration 0, the concentration of all the species will remain 0 for all time. A minimal siphon is one that does not contain a siphon as a subset.
 """
 
-function minimalsiphons(rs::ReactionSystem, algorithm=:SMT) 
+function minimalsiphons(rs::ReactionSystem; algorithm=:SMT) 
     if algorithm == :SMT 
         return minimalsiphons_smt(rs)
-    else
+    elseif algorithm == :ALG
         return minimalsiphons_alg(rs)
+    else 
+        error("Invalid algorithm specified")
     end
 end
-
-# reimplement using Z3 - Satisfiability is a no-go (requires Z3 to be installed)
 
 function minimalsiphons_smt(rs::ReactionSystem) 
     ns = numspecies(rs)
@@ -56,23 +60,21 @@ function minimalsiphons_smt(rs::ReactionSystem)
     status = sat!(constraints..., solver=Z3())
 
     while status == :SAT
-        siphon = findall(value(species))
+        siphon = findall(value(specs))
         push!(siphons, siphon)
-        push!(constraints, or(not.(species[siphon])))
+        push!(constraints, or(not.(specs[siphon])))
         status = sat!(constraints..., solver=Z3())
     end
 
     return siphons
 end
 
-using Oscar
-
-# Check if this can handle open reaction networks
+# TODO: Check if this can handle open reaction networks
 function minimalsiphons_alg(rs::ReactionSystem) 
     sm = speciesmap(rs)
     specs = species(rs)
-    rxns = reactions(rs)
     complexes, D = reactioncomplexes(rs)
+    rxns = reactions(rs)
 
     R, vars = polynomial_ring(QQ, string.(species(rs)))
 
@@ -104,8 +106,52 @@ end
     Checks if a siphon is critical, meaning that it does not contain the support of some conservation law. A reaction network with a critical siphon cannot be persistent.
 """
 function iscritical(s::Vector, conslaws) 
-    supports = [findall(!=(0), conslaws[i, :]) for i in 1:length(claws)]
+    supports = [findall(!=(0), conslaws[i, :]) for i in 1:size(conslaws, 1)]
 
     # If the support of any conservation law is contained in the siphon, then it is not critical
     all(sup->!issubset(sup, s), supports)
 end
+
+"""
+    cycles(rs::ReactionSystem)
+
+    Returns the matrix of cycles (or flux vectors), or reaction fluxes at steady state. These correspond to right eigenvectors of the stoichiometric matrix. Equivalent to [`fluxmodebasis`](@ref). 
+"""
+
+function cycles(rs::ReactionSystem) 
+    # nps = get_networkproperties(rs)
+    nsm = netstoichmat(rs)
+    cycles(nsm)
+    # !isempty(nps.cyclemat) && return nps.cyclemat
+    # nps.cyclemat = cycles(nsm; col_order = nps.col_order)
+    # nps.cyclemat
+end
+
+function cycles(nsm::T; col_order = nothing) where {T <: AbstractMatrix}
+
+    # compute the left nullspace over the integers
+    N = MT.nullspace(nsm; col_order)
+
+    # if all coefficients for a cycle are negative, make positive
+    for Nrow in eachcol(N)
+        all(r -> r <= 0, Nrow) && (Nrow .*= -1)
+    end
+
+    # check we haven't overflowed
+    iszero(nsm * N) || error("Calculation of the cycle matrix was inaccurate, "
+          * "likely due to numerical overflow. Please use a larger integer "
+          * "type like Int128 or BigInt for the net stoichiometry matrix.")
+
+    T(N)
+end
+
+"""
+    fluxvectors(rs::ReactionSystem)
+
+    See documentation for [`cycles`](@ref). 
+"""
+
+function fluxvectors(rs::ReactionSystem) 
+    cycles(rs)
+end
+
